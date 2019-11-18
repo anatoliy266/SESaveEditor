737e6ab415f3fa3f9a930326de47ead39f9b0a44
anatoliy266 <stakan993@gmail.com>
2019-11-19 07:10:16 +0800

    1

---
+++ SESaveEditor/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Configuration;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.XPath;
+using System.Text.RegularExpressions;
+using LiquidTechnologies.GeneratedLx.Ns;
+
+namespace SESaveEditor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string FileName = "";
+
+        private static XmlSerializerNamespaces getAllNamespaces(XmlDocument xDoc)
+        {
+            XmlSerializerNamespaces result = new XmlSerializerNamespaces();
+
+            IDictionary<string, string> localNamespaces = null;
+            XPathNavigator xNav = xDoc.CreateNavigator();
+            while (xNav.MoveToFollowing(XPathNodeType.Element))
+            {
+                localNamespaces = xNav.GetNamespacesInScope(XmlNamespaceScope.Local);
+                foreach (var localNamespace in localNamespaces)
+                {
+                    string prefix = localNamespace.Key;
+                    if (string.IsNullOrEmpty(prefix))
+                        prefix = "";
+
+                    result.Add(prefix, localNamespace.Value);
+                }
+            }
+
+            return result;
+        }
+
+        public static string RemoveTypeTagFromXml(string xml)
+        {
+            if (!string.IsNullOrEmpty(xml) && xml.Contains("xsi:type"))
+            {
+                xml = Regex.Replace(xml, @"\s+xsi:type=""\w+""", "");
+            }
+            return xml;
+        }
+
+        private static T Deserialize<T>(string xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            XmlReaderSettings settings = new XmlReaderSettings();
+            using (StringReader textReader = new StringReader(RemoveTypeTagFromXml(xml)))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(textReader, settings))
+                {
+                    
+                    return (T)serializer.Deserialize(xmlReader);
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Load_File(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                FileName = dialog.FileName;
+                var editFilePath = System.IO.Path.Combine(ConfigurationManager.AppSettings["edited_files_path"], dialog.FileName.Split('/').Last() + "_edited");
+                if (!File.Exists(editFilePath))
+                    File.Copy(dialog.FileName, editFilePath);
+                MyObjectBuilder_CheckpointElm checkpoint;
+                XmlSerializerNamespaces fileNameSpaces;
+                var content = "";
+                using (FileStream fs = File.OpenRead(editFilePath))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        content = sr.ReadToEnd();
+                        var xmldoc = new XmlDocument();
+                        xmldoc.LoadXml(content);
+                        fileNameSpaces = getAllNamespaces(xmldoc);
+                    }
+                }
+                checkpoint = Deserialize<MyObjectBuilder_CheckpointElm>(content);
+                
+                using (var fsv = File.Create(System.IO.Path.Combine(ConfigurationManager.AppSettings["edited_files_path"], dialog.FileName.Split('/').Last() + "_edited1")))
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(MyObjectBuilder_CheckpointElm));
+                    ser.Serialize(new StreamWriter(fsv), checkpoint, fileNameSpaces);
+                }
+                
+            }
+        }
+    }
+}
---
+++ SESaveEditor/Models/SaveModel.cs
@@ -0,0 +1,176 @@
+﻿using System.Xml.Serialization;
+
+
+namespace SESaveEditor.Models
+{
+    public class MyObjectBuilder_Checkpoint
+    {
+        public CurrentSector CurrentSector { get; set; }
+        public string ElapsedGameTime { get; set; }
+        public string SessionName { get; set; }
+        public SpectatorPosition SpectatorPosition { get; set; }
+        public SpectatorSpeed SpectatorSpeed { get; set; }
+        public string SpectatorIsLightOn { get; set; }
+        public string CameraController { get; set; }
+        public string CameraEntity { get; set; }
+        public string ControlledObject { get; set; }
+        public string Password { get; set; }
+        public Description Description { get; set; }
+        public string LastSaveTime { get; set; }
+        public string SpectatorDistance { get; set; }
+        public CharacterToolbar CharacterToolbar { get; set; }
+        public ControlledEntities ControlledEntities { get; set; }
+        public Settings Settings { get; set; }
+        public ScriptManagerData LastSaScriptManagerDataveTime { get; set; }
+        public string AppVersion { get; set; }
+        public Factions Factions { get; set; }
+        public Mods Mods { get; set; }
+        public PromotedUsers PromotedUsers { get; set; }
+        public CreativeTools CreativeTools { get; set; }
+        public Scenario Scenario { get; set; }
+        public RespawnCooldowns RespawnCooldowns { get; set; }
+        public Identities Identities { get; set; }
+        public string PreviousEnvironmentHostility { get; set; }
+        public AllPlayersData AllPlayersData { get; set; }
+        public AllPlayersColors AllPlayersColors { get; set; }
+        public ChatHistory ChatHistory { get; set; }
+        public FactionChatHistory FactionChatHistory { get; set; }
+        public NonPlayerIdentities NonPlayerIdentities { get; set; }
+        public Gps Gps { get; set; }
+        public SessionComponents SessionComponents { get; set; }
+        public GameDefinition GameDefinition { get; set; }
+        public SessionComponentEnabled SessionComponentEnabled { get; set; }
+        public SessionComponentDisabled SessionComponentDisabled { get; set; }
+        public string InGameTime { get; set; }
+        public MissionTriggers MissionTriggers { get; set; }
+        public CustomSkybox CustomSkybox { get; set; }
+        public VicinityModelsCache VicinityModelsCache { get; set; }
+        public VicinityArmorModelsCache VicinityArmorModelsCache { get; set; }
+        public VicinityVoxelCache VicinityVoxelCache { get; set; }
+        public RemoteAdminSettings RemoteAdminSettings { get; set; }
+    }
+
+    public class RemoteAdminSettings
+    {
+    }
+
+    public class VicinityVoxelCache
+    {
+    }
+
+    public class VicinityArmorModelsCache
+    {
+    }
+
+    public class VicinityModelsCache
+    {
+    }
+
+    public class CustomSkybox
+    {
+    }
+
+    public class MissionTriggers
+    {
+    }
+
+    public class SessionComponentDisabled
+    {
+    }
+
+    public class SessionComponentEnabled
+    {
+    }
+
+    public class GameDefinition
+    {
+    }
+
+    public class SessionComponents
+    {
+    }
+
+    public class Gps
+    {
+    }
+
+    public class NonPlayerIdentities
+    {
+    }
+
+    public class FactionChatHistory
+    {
+    }
+
+    public class ChatHistory
+    {
+    }
+
+    public class AllPlayersColors
+    {
+    }
+
+    public class AllPlayersData
+    {
+    }
+
+    public class Identities
+    {
+    }
+
+    public class RespawnCooldowns
+    {
+    }
+
+    public class Scenario
+    {
+    }
+
+    public class CreativeTools
+    {
+    }
+
+    public class PromotedUsers
+    {
+    }
+
+    public class Mods
+    {
+    }
+
+    public class Factions
+    {
+    }
+
+    public class ScriptManagerData
+    {
+    }
+
+    public class Settings
+    {
+    }
+
+    public class ControlledEntities
+    {
+    }
+
+    public class CharacterToolbar
+    {
+    }
+
+    public class Description
+    {
+    }
+
+    public class SpectatorSpeed
+    {
+    }
+
+    public class SpectatorPosition
+    {
+    }
+
+    public class CurrentSector
+    {
+    }
+}
